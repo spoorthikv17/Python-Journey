4eb80a03f7ceaee47eedb447de6127865e9c5905
spoorthikv17 <spoorthikv.feb17@gmail.com>
2026-08-03 20:03:10 +0530

    Day 1: Learn Python basics and practice input/output

---
+++ day1_ip_op.py
@@ -0,0 +1,40 @@
+print("Hellooo, World!")
+
+Name = "Spoorthi"
+college = "Mce"
+Age = 19
+print(Name , college , Age)
+
+
+name = input("Enter your name:")
+print("Welcome!", name , "!")
+
+
+name = input("Enter your name:")
+age = int(input("enter your age:"))
+print(f"Your name is {name} And im {age} years old")
+
+#add two numbers
+a = 10
+b = 20 
+print(a+b)
+
+#area of a rectangle
+length = int(input("Enter the length of rectangle:"))
+breadth = int(input("Enter the breadth of rectangle:"))
+area = length * breadth
+print("Area of rectangle is:", area)
+
+#calculate age
+birth_year = int(input("enter your birth year:"))
+current_year = 2026
+age = current_year - birth_year
+print(age)
+
+#student details
+print("-----Student Details----")
+name = input("Name:")
+age = int(input("Age:"))
+city = input("City:")
+
+

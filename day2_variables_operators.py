fe8cdb635c4b14de3666e3b1ea70788fa6224f8b
spoorthikv17 <spoorthikv.feb17@gmail.com>
2026-08-04 19:13:35 +0530

    Day 2: Practice variables and operators

---
+++ day2_variables_operators.py
@@ -0,0 +1,45 @@
+#Create a variable name and store your name. Print it.
+
+name = "Spoorthi"
+print(name)
+
+#Create variables: age ,college ,branch
+age = 19
+college ="Mce"
+branch = "Csbs"
+print(age , college, branch)
+
+#Store your favorite number in a variable and print:
+
+num = int(input("Enter your fav number:"))
+print(f"your fav num is {num}")
+
+#
+x = 10
+x = 20 #print this bcoz we have updated the value of x
+print(x)
+
+#operators
+a = 10
+b = 20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #give remainder
+print(a**b) #power
+print(a//b) #give quotient integer division
+c = a + b
+print(f"my fav num is {c}")
+print(a==b)
+a += b
+print(a)
+
+#bitwise operators
+x = 10
+y = 4
+print("And:",x & y)
+print("Or:",x | y)
+print("XOR:",x ^ y)
+print("Left Shift:",x << 2)
+print("Right Shift:",x >> 2)
